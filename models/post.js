cfa4c89a9571d954e90a92c413a0440f50dc69b4
mrjackmaster <jacektrg+github@gmail.com>
2022-02-18 00:33:59 +0100

    Adding comments array. Changing min and max lengths of title and text

--- models/post.js
+++ models/post.js
@@ -5,8 +5,9 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema(
 	{
 		author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-		title: { type: String, minlength: 8, maxlength: 64, required: true },
-		text: { type: String, minlength: 8, maxlength: 64, required: true },
+		title: { type: String, minlength: 4, maxlength: 32, required: true },
+		text: { type: String, minlength: 8, maxlength: 512, required: true },
+		comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 		likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 	},
 	{ timestamps: true }
