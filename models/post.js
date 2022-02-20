dc3f055339dbaf1e7d987e80b0f77e236f24212f
mrjackmaster <jacektrg+github@gmail.com>
2022-02-20 18:58:45 +0100

    Removing title from posts

--- models/post.js
+++ models/post.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema(
 	{
 		author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-		title: { type: String, minlength: 4, maxlength: 32, required: true },
 		text: { type: String, minlength: 8, maxlength: 512, required: true },
 		comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 		likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
