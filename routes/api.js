0b0a80bca13f5d8c8672f7c95b3684c6438c647f
mrjackmaster <jacektrg+github@gmail.com>
2022-03-02 21:53:59 +0100

    Adding routes for returning not befriended users, sending friend requests. Fix and cleanup of wrong passport function use for protecting routes.

--- routes/api.js
+++ routes/api.js
@@ -24,131 +24,99 @@ router.get('/log-out', user_controller.log_out_user);
 // Sign up user
 router.post('/sign-up', user_controller.sign_up_user);
 
-// Get user's friend list
-router.get(
-	'/user/user-friend-list',
+/////
+//
+router.use(
 	passport.authenticate('jwt', { session: false }),
-	user_controller.get_user_friend_list
+	(req, res, next) => {
+		if (req.isAuthenticated()) {
+			return next();
+		}
+		res.redirect('/');
+	}
 );
+/////
+
+// Get list of people not befriended by user
+router.get('/users', user_controller.get_not_friend_user_list);
+
+// Send friend request
+router.put('/users/:userid/send-request', user_controller.send_request);
+
+// Get user's friend list
+router.get('/users/user-friend-list', user_controller.get_user_friend_list);
 
 // Get user's friend requests
 router.get(
-	'/user/user-friend-requests',
-	passport.authenticate('jwt', { session: false }),
+	'/users/user-friend-requests',
 	user_controller.get_user_friend_requests
 );
 
-// Get user's friend requests
-router.get(
-	'/user/user-friends-data',
-	passport.authenticate('jwt', { session: false }),
-	user_controller.get_user_friends_data
-);
+// Get user's friend list and friend requests
+router.get('/users/user-friends-data', user_controller.get_user_friends_data);
 
-// Get user's friend list
-router.post(
-	'/user/accept-request/:requestid',
-	passport.authenticate('jwt', { session: false }),
+// Update user's friend list after accepting
+router.put(
+	'/users/accept-request/:requestid',
 	user_controller.accept_friend_request
 );
 
-// Get user's friend requests
-router.post(
-	'/user/decline-request/:requestid',
-	passport.authenticate('jwt', { session: false }),
+// Update user's friend requests after declining
+router.put(
+	'/users/decline-request/:requestid',
 	user_controller.decline_friend_request
 );
 
 /////
 // Get user's posts
-router.get(
-	'/posts/user-posts',
-	passport.authenticate('jwt', { session: false }),
-	post_controller.get_user_posts
-);
+router.get('/posts/user-posts', post_controller.get_user_posts);
 
 // Get user's friends posts
-router.get(
-	'/posts/user-friends-posts',
-	passport.authenticate('jwt', { session: false }),
-	post_controller.get_user_friends_posts
-);
+router.get('/posts/user-friends-posts', post_controller.get_user_friends_posts);
 
 // Get user's timeline posts
 router.get(
 	'/posts/user-timeline-posts',
-	passport.authenticate('jwt', { session: false }),
 	post_controller.get_user_timeline_posts
 );
 
 // Create new post
-router.post(
-	'/posts',
-	passport.authenticate('jwt', { session: false }),
-	post_controller.create_post
-);
+router.post('/posts', post_controller.create_post);
 
 // Get single post
-router.get(
-	'/posts/:postid',
-	passport.authenticate('jwt', { session: false }),
-	post_controller.get_single_post
-);
+router.get('/posts/:postid', post_controller.get_single_post);
 
 // Update a post
-router.put(
-	'/posts/:postid',
-	passport.authenticate('jwt', { session: false }),
-	post_controller.update_post
-);
+router.put('/posts/:postid', post_controller.update_post);
 
 // Delete a post
-router.delete(
-	'/posts/:postid',
-	passport.authenticate('jwt', { session: false }),
-	post_controller.delete_post
-);
+router.delete('/posts/:postid', post_controller.delete_post);
 
 // Like a post
-router.put(
-	'/posts/like/:postid',
-	passport.authenticate('jwt', { session: false }),
-	post_controller.change_like_status
-);
+router.put('/posts/like/:postid', post_controller.change_like_status);
 
 /////
 // Create new comment
-router.post(
-	'/posts/:postid/comments',
-	passport.authenticate('jwt', { session: false }),
-	comment_controller.create_comment
-);
+router.post('/posts/:postid/comments', comment_controller.create_comment);
 
 // Get post comments
-router.get(
-	'/posts/:postid/comments',
-	passport.authenticate('jwt', { session: false }),
-	comment_controller.get_post_comments
-);
+router.get('/posts/:postid/comments', comment_controller.get_post_comments);
 
 // Update a comment
 router.put(
 	'/posts/:postid/comments/:commentid',
-	passport.authenticate('jwt', { session: false }),
 	comment_controller.update_comment
 );
 
 // Delete a comment
 router.delete(
 	'/posts/:postid/comments/:commentid',
-	passport.authenticate('jwt', { session: false }),
 	comment_controller.delete_comment
 );
 
 // Like a comment
 router.put(
 	'/posts/:postid/comments/:commentid/like',
-	passport.authenticate('jwt', { session: false }),
 	comment_controller.change_like_status
 );
 
