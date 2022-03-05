0f29a2390dcec8a99e5c32341e03c9219e01768f
mrjackmaster <jacektrg+github@gmail.com>
2022-03-06 00:56:19 +0100

    Adding full name virtual. Adding toJSON option to send virtuals to client. Adding select parameter to not return user's password by default on queries

--- models/user.js
+++ models/user.js
@@ -3,25 +3,34 @@ require('mongoose-type-email');
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-	first_name: { type: String, minlength: 4, maxlength: 32, required: true },
-	last_name: { type: String, minlength: 4, maxlength: 32, required: true },
-	email: {
-		type: Schema.Types.Email,
-		minlength: 8,
-		maxlength: 64,
-		required: true,
+const UserSchema = new Schema(
+	{
+		first_name: { type: String, minlength: 4, maxlength: 32, required: true },
+		last_name: { type: String, minlength: 4, maxlength: 32, required: true },
+		email: {
+			type: Schema.Types.Email,
+			minlength: 8,
+			maxlength: 64,
+			required: true,
+		},
+		password: {
+			type: String,
+			minlength: 8,
+			maxlength: 64,
+			required: true,
+			select: false,
+		},
+		friend_list: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+		blocked_user_list: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+		incoming_friend_requests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+		outgoing_friend_requests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 	},
-	password: { type: String, minlength: 8, maxlength: 64, required: true },
-	friend_list: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-	blocked_user_list: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-	incoming_friend_requests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-	outgoing_friend_requests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-});
+	{ toJSON: { virtuals: true } }
+);
 
-// Virtual for post's URL
-UserSchema.virtual('url').get(function () {
-	return '/users/' + this._id;
+// Virtual for user's full name
+UserSchema.virtual('full_name').get(function () {
+	return `${this.first_name} ${this.last_name}`;
 });
 
 module.exports = mongoose.model('User', UserSchema);
