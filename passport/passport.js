301ab16a5d859b77465a7ec9eb444a9f332c2080
mrjackmaster <jacektrg+github@gmail.com>
2022-03-06 00:56:39 +0100

    Updating query to include password

--- passport/passport.js
+++ passport/passport.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const JWTStrategy = require('passport-jwt').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const bcryptjs = require('bcryptjs');
-const User = require('./models/user');
+const User = require('../models/user');
 
 passport.use(
 	'login',
@@ -11,7 +12,9 @@ passport.use(
 		{ usernameField: 'email', passwordField: 'password' },
 		async (email, password, done) => {
 			try {
-				const user = await User.findOne({ email: email });
+				const user = await User.findOne({ email: email })
+					.select('+password')
+					.exec();
 				if (!user) {
 					return done(null, false, { msg: 'Incorrect email' });
 				}
@@ -44,6 +47,8 @@ passport.use(
 		},
 		(jwtPayload, done) => {
 			try {
+				console.log(123);
+
 				if (jwtPayload) {
 					done(null, jwtPayload);
 				} else {
