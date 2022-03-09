ac9c1614af2cdd95d72907f06dad09b05f656200
mrjackmaster <jacektrg+github@gmail.com>
2022-03-09 21:08:04 +0100

    Fixing populate on new comment.

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -37,7 +37,7 @@ exports.create_comment = [
 				{ new: true }
 			)
 				.populate('author')
-				.populate('comments')
+				.populate({ path: 'comments', populate: { path: 'author' } })
 				.exec();
 			return res.status(200).json(post);
 		} catch (error) {
