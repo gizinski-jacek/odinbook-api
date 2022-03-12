ecffd8dccbf028b4b6c38dcc1424bac542c6c989
mrjackmaster <jacektrg+github@gmail.com>
2022-03-12 18:14:04 +0100

    Removing get single post controller, moving things around.

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,6 +5,27 @@ const { body, validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const url = require('url');
 
+exports.get_user_timeline_posts = async (req, res, next) => {
+	try {
+		const user = await User.findById(req.user._id).exec();
+		const timeline_post_list = await Post.find({
+			author: { $in: [user._id, ...user.friend_list] },
+		})
+			.sort({ createdAt: 'desc' })
+			.populate('author')
+			.populate({
+				path: 'comments',
+				populate: {
+					path: 'author',
+				},
+			})
+			.exec();
+		return res.status(200).json(timeline_post_list);
+	} catch (error) {
+		next(error);
+	}
+};
+
 exports.create_post = [
 	body('text', 'Text is invalid')
 		.trim()
@@ -40,61 +61,6 @@ exports.create_post = [
 		}
 	},
 ];
-
-exports.get_user_post_list = async (req, res, next) => {
-	try {
-		if (!mongoose.Types.ObjectId.isValid(req.params.userid)) {
-			return res.status(404).json('Invalid post Id');
-		}
-		const post_list = await Post.find({ author: req.params.userid })
-			.sort({ createdAt: 'desc' })
-			.populate('author')
-			.populate('comments')
-			.exec();
-		return res.status(200).json(post_list);
-	} catch (error) {
-		next(error);
-	}
-};
-
-exports.get_single_post = async (req, res, next) => {
-	try {
-		if (!mongoose.Types.ObjectId.isValid(req.params.postid)) {
-			return res.status(404).json('Invalid post Id');
-		}
-		const post = await Post.findById(req.params.postid)
-			.populate('author')
-			.populate('comments')
-			.exec();
-		if (!post) {
-			return res.status(404).json('Post not found');
-		}
-		return res.status(200).json(post);
-	} catch (error) {
-		next(error);
-	}
-};
-
-exports.get_user_timeline_posts = async (req, res, next) => {
-	try {
-		const user = await User.findById(req.user._id).exec();
-		const timeline_post_list = await Post.find({
-			author: { $in: [user._id, ...user.friend_list] },
-		})
-			.sort({ createdAt: 'desc' })
-			.populate('author')
-			.populate({
-				path: 'comments',
-				populate: {
-					path: 'author',
-				},
-			})
-			.exec();
-		return res.status(200).json(timeline_post_list);
-	} catch (error) {
-		next(error);
-	}
-};
 
 exports.update_post = [
 	body('text', 'Text is invalid')
