0c6eb34fc638d986f82fc29d927934be578a2ef6
mrjackmaster <jacektrg+github@gmail.com>
2022-03-14 17:24:43 +0100

    Adding passport facebook strategy. Returns user with existing facebook link, else checks if email from facebook account is taken and links existing account with fb, else uses provided data from fb and creates new user with random password.

--- passport/passport.js
+++ passport/passport.js
@@ -30,6 +30,53 @@ passport.use(
 	)
 );
 
+passport.use(
+	'facebook',
+	new FacebookStrategy(
+		{
+			clientID: process.env.FACEBOOK_APP_ID,
+			clientSecret: process.env.FACEBOOK_SECRET,
+			callbackURL: 'http://localhost:4000/api/log-in/facebook/callback',
+			profileFields: ['id', 'email', 'displayName'],
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			try {
+				const userExists = await User.findOne({ facebookId: profile.id })
+					.select('+email')
+					.exec();
+				if (!userExists) {
+					const emailUsed = await User.findOne({
+						email: profile.emails[0].value,
+					}).exec();
+					if (!emailUsed) {
+						const newUser = new User({
+							facebookId: profile.id,
+							first_name: profile.displayName.split(' ')[0],
+							last_name: profile.displayName.split(' ')[1],
+							email: profile.emails[0].value,
+						});
+						const randomString = Math.random().toString(36).substring(2, 10);
+						const hashedPassword = await bcryptjs.hash(randomString, 10);
+						newUser.password = hashedPassword;
+						const user = await newUser.save();
+						return done(null, user);
+					}
+					const linkFBToAccount = await User.findByIdAndUpdate(
+						emailUsed._id,
+						{ facebookId: profile.id },
+						{ new: true }
+					);
+					return done(null, linkFBToAccount);
+				}
+				return done(null, userExists);
+			} catch (error) {
+				console.log(error);
+				done(error);
+			}
+		}
+	)
+);
+
 const extractCookie = (req) => {
 	let token = null;
 	if (req && req.cookies && req.cookies.token) {
