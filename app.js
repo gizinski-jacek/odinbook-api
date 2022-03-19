5f9520d4682b604f6daf04ff5992e0624e673175
mrjackmaster <jacektrg+github@gmail.com>
2022-03-19 19:17:58 +0100

    Serving uploaded  files

--- app.js
+++ app.js
@@ -19,14 +19,13 @@ const indexRouter = require('./routes/index');
 const apiRouter = require('./routes/api');
 
 const app = express();
-
 app.use(cors());
 app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
 
 app.use('/', indexRouter);
