1c38684aa08de33fb8f2298a15fa0d38edcc7632
mrjackmaster <jacektrg+github@gmail.com>
2022-03-22 00:48:01 +0100

    Schema for chat message containing author, message contents and reference to chatroom

---
+++ models/message.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema(
+	{
+		chat_ref: { type: Schema.Types.ObjectId, ref: 'Chat', required: true },
+		author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+		text: { type: String, minlength: 1, maxlength: 64, required: true },
+	},
+	{ timestamps: true }
+);
+
+module.exports = mongoose.model('Message', MessageSchema);
