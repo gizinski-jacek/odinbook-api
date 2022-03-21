464f5d5f43151ed14ae8e27d3c306722d5fd7608
mrjackmaster <jacektrg+github@gmail.com>
2022-03-22 00:48:36 +0100

    Schema for chatroom containinng array of participants id's and array of message's id's

---
+++ models/chat.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ChatSchema = new Schema({
+	participants: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+	message_list: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
+});
+
+module.exports = mongoose.model('Chat', ChatSchema);
