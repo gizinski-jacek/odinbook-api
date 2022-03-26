c9bd7ef88073a9fb165625c880029e6c31723129
mrjackmaster <jacektrg+github@gmail.com>
2022-03-27 00:00:56 +0100

    Adding controllers for getting chat messages, getting new received messages list and marking messages as viewed

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -7,7 +7,35 @@ const url = require('url');
 const fs = require('fs');
 const { socketEmits } = require('../socketio/socketio');
 
-exports.create_message = [
+exports.get_chat_message_list = async (req, res, next) => {
+	try {
+		req.query.participants.forEach((participant) => {
+			if (!mongoose.Types.ObjectId.isValid(participant)) {
+				return res.status(404).json('Invalid post Id');
+			}
+		});
+		const chatExists = await Chat.findOne({
+			participants: req.query.participants,
+		})
+			.populate({
+				path: 'message_list',
+				populate: {
+					path: 'author',
+				},
+			})
+			.exec();
+		if (!chatExists) {
+			const newChat = new Chat({ participants: participants });
+			const chat = await newChat.save();
+			return res.status(200).json(chat);
+		}
+		return res.status(200).json(chatExists);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+exports.create_chat_message = [
 	body('text', 'Text is invalid').trim().isLength({ min: 1, max: 64 }).escape(),
 	async (req, res, next) => {
 		try {
@@ -38,10 +66,63 @@ exports.create_message = [
 			if (!errors.isEmpty()) {
 				return res.status(404).json(errors.array());
 			}
-			socketEmits.send_message(req.user._id, chatData);
+			socketEmits.send_message(req.body.recipient, chatData);
 			return res.status(200).json('Chat message sent');
 		} catch (error) {
 			next(error);
 		}
 	},
 ];
+
+exports.get_new_message_list = async (req, res, next) => {
+	try {
+		const new_message_list = await Message.find({
+			$and: [
+				{ participants: { $in: req.user._id } },
+				{
+					author: { $ne: req.user._id },
+				},
+				{
+					viewed: false,
+				},
+			],
+		})
+			.sort({ createdAt: 'desc' })
+			.populate('author')
+			.exec();
+		return res.status(200).json(new_message_list);
+	} catch (error) {
+		next(error);
+	}
+};
+
+exports.dismiss_message = async (req, res, next) => {
+	try {
+		if (!mongoose.Types.ObjectId.isValid(req.body.messageId)) {
+			return res.status(404).json('Invalid message Id');
+		}
+		const message = await Message.findByIdAndUpdate(req.body.messageId, {
+			viewed: true,
+		}).exec();
+		if (!message) {
+			return res.status(404).json('Message not found');
+		}
+		const new_message_list = await Message.find({
+			$and: [
+				{ participants: { $in: req.user._id } },
+				{
+					author: { $ne: req.user._id },
+				},
+				{
+					viewed: false,
+				},
+			],
+		})
+			.sort({ createdAt: 'desc' })
+			.populate('author')
+			.exec();
+		return res.status(200).json(new_message_list);
+	} catch (error) {
+		next(error);
+	}
+};
