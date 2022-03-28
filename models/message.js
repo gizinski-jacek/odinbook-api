6c19f5601f4f719e4bf69d3b09bae4dea9862661
mrjackmaster <jacektrg+github@gmail.com>
2022-03-28 21:05:30 +0200

    Changing read property of message to array of ID's of user who read the message

--- models/message.js
+++ models/message.js
@@ -7,7 +7,7 @@ const MessageSchema = new Schema(
 		chat_ref: { type: Schema.Types.ObjectId, ref: 'Chat', required: true },
 		author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 		text: { type: String, minlength: 1, maxlength: 64, required: true },
-		read: { type: Boolean, default: false },
+		readBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 	},
 	{ timestamps: true }
 );
