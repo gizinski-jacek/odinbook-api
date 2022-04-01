650476f43ef576849752cab0fed0f1625080c489
mrjackmaster <jacektrg+github@gmail.com>
2022-04-01 23:22:33 +0200

    Adding picture property to post schema.

--- models/post.js
+++ models/post.js
@@ -6,6 +6,7 @@ const PostSchema = new Schema(
 	{
 		author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 		text: { type: String, minlength: 1, maxlength: 512, required: true },
+		picture: { type: String, maxlength: 128, default: '' },
 		comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 		likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 	},
