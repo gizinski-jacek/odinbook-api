a41d93cc9980c4ea1d1eabc4d001750b5fedaf92
mrjackmaster <jacektrg+github@gmail.com>
2022-04-05 18:59:55 +0200

    Adding timestamps to chat schema

--- models/chat.js
+++ models/chat.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const ChatSchema = new Schema({
-	participants: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
-	message_list: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
-});
+const ChatSchema = new Schema(
+	{
+		participants: [
+			{ type: Schema.Types.ObjectId, ref: 'User', required: true },
+		],
+		message_list: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('Chat', ChatSchema);
